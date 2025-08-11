6413cff6f58a5b4e074b48e73b23e5ab1c4487a3
Lauris BH <lauris@nix.lv>
2025-08-11 17:27:46 +0300

    Add option to replace content in static embedded files

--- static.go
+++ static.go
@@ -1,7 +1,9 @@
 package azugo
 
 import (
+	"bytes"
 	"embed"
+	"io"
 	"io/fs"
 	"mime"
 	"path/filepath"
@@ -11,6 +13,9 @@ import (
 type staticOptions struct {
 	// TrimPrefix is the prefix to trim from the FS path.
 	TrimPrefix string
+
+	// Replacer will be used to replace values in the content of all files.
+	Replacer *strings.Replacer
 }
 
 type StaticOption interface {
@@ -22,6 +27,17 @@ type StaticDirTrimPrefix string
 
 func (p StaticDirTrimPrefix) apply(o *staticOptions) {
 	o.TrimPrefix = string(p)
+}
+
+type valueReplacer []string
+
+func (r valueReplacer) apply(o *staticOptions) {
+	o.Replacer = strings.NewReplacer(r...)
+}
+
+// StaticContentReplacer sets the replacer for the static file content.
+func StaticContentReplacer(oldnew ...string) StaticOption {
+	return valueReplacer(oldnew)
 }
 
 func (a *App) StaticEmbedded(path string, f embed.FS, opts ...StaticOption) error {
@@ -39,6 +55,9 @@ func (a *App) StaticEmbedded(path string, f embed.FS, opts ...StaticOption) erro
 		base += "/"
 	}
 
+	altcontent := make(map[string][]byte, 5)
+	extcache := make(map[string]string, 10)
+
 	if err := fs.WalkDir(f, ".", func(file string, d fs.DirEntry, err error) error {
 		if err != nil || d.IsDir() {
 			return err
@@ -51,8 +70,36 @@ func (a *App) StaticEmbedded(path string, f embed.FS, opts ...StaticOption) erro
 			fpath += file
 		}
 
+		// Check if the content needs to be replaced
+		if opt.Replacer != nil {
+			go func(file string, f embed.FS) {
+				s, err := f.Open(file)
+				if err != nil {
+					return
+				}
+				defer s.Close()
+
+				buf := new(bytes.Buffer)
+				if _, err := io.Copy(buf, s); err != nil {
+					return
+				}
+
+				content := []byte(opt.Replacer.Replace(buf.String()))
+				if !bytes.Equal(content, buf.Bytes()) {
+					altcontent[fpath] = content
+				}
+			}(file, f)
+		}
+
+		extcache[fpath] = mime.TypeByExtension(filepath.Ext(fpath))
+
 		a.Get(fpath, func(ctx *Context) {
-			ctx.Header.Set("Content-Type", mime.TypeByExtension(filepath.Ext(path)))
+			ctx.Header.Set("Content-Type", extcache[fpath])
+			if content, ok := altcontent[fpath]; ok {
+				ctx.Raw(content)
+				return
+			}
+
 			s, err := f.Open(file)
 			if err != nil {
 				ctx.Error(err)
